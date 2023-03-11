b29a0bc463a336215fec13da20bfeca32fe13581
bouboumael <bouboumael@orange.fr>
2023-03-11 02:31:15 +0100

    server component

--- src/app/(groupe)/hello/page.tsx
+++ src/app/(groupe)/hello/page.tsx
@@ -1,32 +1,11 @@
-'use client';
+import ContentPage from "@/app/(groupe)/hello/ContentPage";
+import {Metadata} from "next";
 
-import Link from "next/link";
-import {useEffect, useState} from "react";
+export const metadata: Metadata = {
+    title: 'next test mael',
+    description: 'ma première app next'
+}
 
 export default function Page() {
-
-    const [api, setApi] = useState()
-
-    const fetchData = async() =>{
-        const data = await fetch('/api/hello')
-        const json = await data.json();
-        setApi(json)
-    }
-
-    useEffect(()=>{
-        fetchData()
-    },[])
-
-    useEffect(()=>{
-        console.log(api)
-    },[api])
-
-    return (
-        <>
-            <Link href="/">
-                Accueil
-            </Link>
-            <h1>Hello, Maël 2</h1>
-        </>
-    );
+    return <ContentPage/>;
 }
